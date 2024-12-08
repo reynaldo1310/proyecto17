8bd57225ec096743e97a346528bdb081b2d6a199
Reynaldo Contreras <reycontrerassayan@gmail.com>
2024-12-09 00:44:06 +0100

    Subimos ejercicio aún con partes sin terminar

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,41 @@
-$(document).ready(function() {
-    solicitudes=["Mensaje", "Hola", "Adios"];
+$(document).ready(function () {
 
-    for(i= 0 ; i < solicitudes.length; i++) {
-        $("#maestro").append(
-            $("<li>").text(solicitudes[i])
-        );
-    }
+    // Tabla de Arrays 
+    let solicitudes = [{
+        "id": 1,
+        "nombre": "Juan",
+        "apellido": "Secreto"
+        }, {
+        "id": 2,
+        "nombre": "Antonio",
+        "apellido": "Pero"
+        }, {
+        "id": 3,
+        "nombre": "de la Encarnación",
+        "apellido": "No tanto"
+         }, {
+        "id": 4,
+        "nombre": "Rey",
+        "apellido": "Contreras"
+        }
+        ];
+
+
+    // Evento click para que cargue el array en tabla
+$("#boton_maestro").on('click', function (event) {
+    let $maestro = $('#maestro tbody');
+
+    for (i = 0; i < solicitudes.length; i++) {
+        let $linea = $('<tr>');
+        $linea.append($('<td>').text(solicitudes[i].id));
+        $linea.append($('<td>').text(solicitudes[i].nombre));
+        $linea.append($('<td>').text(solicitudes[i].apellido));
+        $maestro.append($linea);
+        }
+
+    $("#boton_maestro").hide();
+    $("#boton_texto").show();
+    
+    
+    });
 });
